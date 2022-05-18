fb3f2ce65a05d702c2c3e796206fd3ffdd456c6d
Ignacio Yz <ignacioyz@gmail.com>
2022-05-17 20:23:40 -0400

    commit tree 56d51e442509a37cf0246a63a07f956669aacfc8

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+// const returnFirstTwoDrivers = (param) => {
+//     return param.slice(0, 2)
+// }
+
+// const returnLastTwoDrivers = (param) => {
+//     return param.slice(2)
+// }
+
+// const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+// function createFareMultiplier()
+//const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+// returnFirstTwoDrivers() — Declare the variable returnFirstTwoDrivers with const 
+//and assign an anonymous function to it. The assigned function should accept an array
+// of drivers as an argument and return the first two drivers in the array.
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(intiger) {
+    return function (fare) {
+        return fare * intiger
+    }
+}
+
+const fareDoubler = function (fare){
+    return fare * 2 
+}
+
+const fareTripler = function(fare){
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, driverOptions){
+    return driverOptions(drivers)
+}
+
+
+
+
